6433a79587393e777a5aa5bb60eadd96ebb71d5b
Ayub Wicaksono <ayubwicak27@gmail.com>
2025-09-14 01:01:47 +0700

    1 mastering profile (#5)
    * contoh
    
    * lanjutan

--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,18 @@ Route::prefix('blog')->name('blog.')->group(function () {
     Route::put('/{id}', UpdateBlogController::class);
     Route::delete('/{id}', DeleteBlogController::class);
 });
+
+// Auto-generated for Profile
+use App\Http\Controllers\Api\profile\CreateProfileController;
+use App\Http\Controllers\Api\profile\GetAllProfileController;
+use App\Http\Controllers\Api\profile\GetOneProfileController;
+use App\Http\Controllers\Api\profile\UpdateProfileController;
+use App\Http\Controllers\Api\profile\DeleteProfileController;
+
+Route::prefix('profile')->name('profile.')->group(function () {
+    Route::get('/', GetAllProfileController::class);
+    Route::get('/{id}', GetOneProfileController::class);
+    Route::post('/', CreateProfileController::class);
+    Route::put('/{id}', UpdateProfileController::class);
+    Route::delete('/{id}', DeleteProfileController::class);
+});
