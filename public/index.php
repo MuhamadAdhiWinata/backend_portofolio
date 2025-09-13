56a2198011649eab3bec21345611895aa55eda15
Muhamad Adhi Winata <muhamadadhiw@gmail.com>
2025-09-14 00:15:29 +0700

    set deployment

--- public/index.php
+++ public/index.php
@@ -1,3 +1,4 @@
+<!-- dev -->
 <?php
 
 use Illuminate\Foundation\Application;
@@ -6,15 +7,38 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
+
+
+// production
+// <?php
+
+// use Illuminate\Foundation\Application;
+// use Illuminate\Http\Request;
+
+// define('LARAVEL_START', microtime(true));
+
+// // Determine if the application is in maintenance mode...
+// if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+//     require $maintenance;
+// }
+
+// // Register the Composer autoloader...
+// require __DIR__ . '/../backend_portofolio/vendor/autoload.php';
+
+// // Bootstrap Laravel and handle the request...
+// /** @var Application $app */
+// $app = require_once __DIR__ . '/../backend_portofolio/bootstrap/app.php';
+
+// $app->handleRequest(Request::capture());
